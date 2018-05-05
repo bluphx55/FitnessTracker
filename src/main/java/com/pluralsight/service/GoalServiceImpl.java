a44cf158476009d3b98495fff66dd58e01242cab
BluPhx55 <bluphx55@gmail.com>
2018-05-05 11:10:46 -0500

    Just finished 7.8 Learning about the Service annotation.

---
+++ src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -0,0 +1,12 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Goal;
+import org.springframework.stereotype.Service;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+    public Goal save(Goal goal) {
+        return null;
+    }
+}
