da769a56581953764af74073f200ab8a2db01af7
BluPhx55 <bluphx55@gmail.com>
2018-05-05 11:17:57 -0500

    Just finished 7.9 Learning about the Repository annotation.

---
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Goal;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository("goalRepository")
+public class GoalRepositoryImpl implements GoalRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Goal save(Goal goal) {
+
+        em.persist(goal);
+
+        return goal;
+    }
+}
--- src/main/java/com/pluralsight/service/GoalServiceImpl.java
+++ src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -1,12 +1,19 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Goal;
+import com.pluralsight.repository.GoalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("goalService")
 public class GoalServiceImpl implements GoalService {
 
+    @Autowired
+    private GoalRepository goalRepository;
+
     public Goal save(Goal goal) {
-        return null;
+
+
+        return goalRepository.save(goal);
     }
 }
